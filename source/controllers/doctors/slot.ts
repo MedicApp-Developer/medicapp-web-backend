85ed82bc9966bcd2586cd2be1b3d699764585a27
Usama0101 <usama.farooq@bigimmersive.com>
2021-12-16 03:38:17 +0500

    slots updated

--- source/controllers/doctors/slot.ts
+++ source/controllers/doctors/slot.ts
@@ -5,11 +5,11 @@ import makeResponse, { sendErrorResponse } from '../../functions/makeResponse';
 const NAMESPACE = "Slot";
 
 const createSlot = async (req: Request, res: Response, next: NextFunction) => {
-        const { from, to, doctorId, hospitalId, appointmentId } = req.body;
+        const { from, to, doctorId, hospitalId } = req.body;
     
-        if(from && to && doctorId && hospitalId && appointmentId) {
+        if(from && to && doctorId && hospitalId) {
             const newSlot = new Slot({
-                from, to, doctorId, hospitalId, appointmentId
+                from, to, doctorId, hospitalId
             });
             newSlot.save().then(result => {
                 return makeResponse(res, 200, "Doctor", result, false);
