f4439d820cd841556529d0be66626a01b80b87b8
Usama Farooq <usama.farooq@bigimmersive.com>
2022-02-25 00:33:02 +0500

    permission changed for promo

--- source/routes/hospitals/promos.ts
+++ source/routes/hospitals/promos.ts
@@ -2,11 +2,12 @@ import express from 'express';
 import controller from '../../controllers/hospitals/promos';
 import isHospital from '../../middleware/isHospital';
 import upload from '../../functions/multerCloudinary';
+import extractJWT from '../../middleware/extractJWT';
 
 const router = express.Router();
 
 router.get('/', isHospital, controller.getAllPromos);
-router.get('/all', isHospital, controller.getAllPromoVideos);
+router.get('/all', extractJWT, controller.getAllPromoVideos);
 router.post('/', isHospital, upload.single("video"), controller.createPromo);
 router.delete('/:id', isHospital, controller.deletePromo);
 
