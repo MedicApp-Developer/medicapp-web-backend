220b5c3695898e977ac44a3d40ce2cf03ad00a2e
Usama Farooq <usama.farooq@bigimmersive.com>
2022-05-16 01:42:00 +0500

    Heroku

--- source/server.ts
+++ source/server.ts
@@ -132,7 +132,6 @@ router.use((req, res, next) => {
         message: error.message
     })
 })
-
 const httpServer = http.createServer(router)
 
 httpServer.listen(config.server.port, () => logging.info(NAMESPACE, `Server is running ${config.server.hostname}:${config.server.port}`))
