e16f2da076eaf3d16f49d8e6b9e96417211ac282
Usama Farooq <usama.farooq@bigimmersive.com>
2022-05-17 00:31:27 +0500

    Heroku

--- source/server.ts
+++ source/server.ts
@@ -135,4 +135,3 @@ router.use((req, res, next) => {
 const httpServer = http.createServer(router)
 
 httpServer.listen(config.server.port, () => logging.info(NAMESPACE, `Server is running ${config.server.hostname}:${config.server.port}`))
-a
